321327f1b4ea623e36eb5e82045a72bc595577d9
RAITILAK <raitilak1212@gmail.com>
2026-04-03 20:20:27 +0530

    fix: update CORS for production

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "https://finance-dashboard-backend-eight.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
